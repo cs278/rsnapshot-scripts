955c1512f6a746c7a140198ef4f1e19bb3dcf864
Chris Smith <chris@cs278.org>
2007-07-15 16:07:36 +0000

    remove crap

--- debian/examples/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the cs-meta package
-#
-0 4	* * *	root	cs-meta_maintenance
--- debian/examples/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(cs-meta):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="cs-meta" command="/usr/bin/cs-meta"
